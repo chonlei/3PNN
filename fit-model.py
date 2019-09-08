1d577b79be3430420802c6828f8f4e536fe3eb4e
Chon Lok Lei <chonloklei@gmail.com>
2019-09-08 16:08:51 +0100

    Add model fitting, prior, parameter transformation

---
+++ fit-model.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python3
+import sys
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import pints
+
+import read
+import model as m
+
+"""
+Run fitting for the cochlea EFI model.
+"""
+
+try:
+    file_id = sys.argv[1]
+except IndexError:
+    print('Usage: python test.py [str:file_id] [int:n_repeats]')
+    sys.exit()
+path2files = 'data'
+filename = path2files + '/' + file_id + '.txt'
+
+savedir = './out'
+if not os.path.isdir(savedir):
+    os.makedirs(savedir)
+
+saveas = file_id
+
+# Control fitting seed
+# fit_seed = np.random.randint(0, 2**30)
+fit_seed = 542811797
+print('Fit seed: ', fit_seed)
+np.random.seed(fit_seed)
+
+# Load data
+raw_data = read.load(filename)
+n_readout, n_stimuli = raw_data.shape
+assert(n_readout == n_stimuli)  # assume it is a sqaure matrix for EFI
+sigma_noise = 0.1  # guess in range of 0.1 kOhms
+
+broken_electrodes = [12, 16]
+
+# Create mask to filter data
+mask = lambda y: read.mask(y, x=broken_electrodes)
+
+# Create model
+model = m.FirstOrderLeakyTransmissionLineNetwork(n_electrodes=n_readout)
+
+# Parameter transformation (remove positive constraint)
+transform_to_model_param = m.log_transform_to_model_param
+transform_from_model_param = m.log_transform_from_model_param
+
+# Prior parameters (initial guess)
+rt = np.ones(n_readout - 1) * 10.  # transversal resistance (electrode order)
+rbasel = 5.  # Basel resistance (next to the last electrode)
+rl = np.ones(n_readout - 1) * 0.5  # longitudinal resistance (electrode order)
+
+modelparams = np.append(np.append(rt, rbasel), rl)
+priorparams = np.append(modelparams, sigma_noise)
+transform_modelparams = transform_from_model_param(modelparams)
+transform_priorparams = np.append(transform_modelparams, sigma_noise)
+
+# Create likelihood, prior, and posterior
+loglikelihood = m.GaussianLogLikelihood(model, raw_data, mask)
+logmodelprior = m.UniformLogPrior(n_electrodes=n_readout,
+        transform=transform_to_model_param,
+        inv_transform=transform_from_model_param)
+lognoiseprior = pints.UniformLogPrior([0.1 * sigma_noise], [10. * sigma_noise])
+logprior = pints.ComposedLogPrior(logmodelprior, lognoiseprior)
+logposterior = pints.LogPosterior(loglikelihood, logprior)
+
+# Check logposterior is working fine
+print('Score at prior parameters: ',
+        logposterior(transform_priorparams))
+for _ in range(10):
+    assert(logposterior(transform_priorparams) ==\
+            logposterior(transform_priorparams))
+
+# Run fits
+try:
+    n_repeats = int(sys.argv[2])
+except IndexError:
+    n_repeats = 3
+
+params, logposteriors = [], []
+
+for i in range(n_repeats):
+
+    if i == 0:
+        x0 = transform_priorparams
+    else:
+        # Randomly pick a starting point
+        x0 = logprior.sample(n=1)[0]
+    print('Starting point: ', x0)
+
+    # Create optimiser
+    print('Starting logposterior: ', logposterior(x0))
+    opt = pints.OptimisationController(logposterior, x0.T, method=pints.CMAES)
+    opt.set_max_iterations(None)
+    opt.set_parallel(True)
+
+    # Run optimisation
+    try:
+        with np.errstate(all='ignore'):
+            # Tell numpy not to issue warnings
+            p, s = opt.run()
+            p[:-1] = transform_to_model_param(p[:-1])  # last one is sigma
+            params.append(p)
+            logposteriors.append(s)
+            print('Found solution:' )
+            for k, x in enumerate(p):
+                print(pints.strfloat(x))
+    except ValueError:
+        import traceback
+        traceback.print_exc()
+
+# Order from best to worst
+order = np.argsort(logposteriors)[::-1]  # (use [::-1] for LL)
+logposteriors = np.asarray(logposteriors)[order]
+params = np.asarray(params)[order]
+
+# Show results
+bestn = min(3, n_repeats)
+print('Best %d logposteriors:' % bestn)
+for i in xrange(bestn):
+    print(logposteriors[i])
+print('Mean & std of logposterior:')
+print(np.mean(logposteriors))
+print(np.std(logposteriors))
+print('Worst logposterior:')
+print(logposteriors[-1])
+
+# Extract best 3
+obtained_logposterior0 = logposteriors[0]
+obtained_parameters0 = params[0]
+obtained_logposterior1 = logposteriors[1]
+obtained_parameters1 = params[1]
+obtained_logposterior2 = logposteriors[2]
+obtained_parameters2 = params[2]
+
+# Show results
+print('Found solution:' )
+# Store output
+with open('%s/%s-solution-%s-1.txt' % (savedir, saveas, fit_seed), 'w') as f:
+    for k, x in enumerate(obtained_parameters0):
+        print(pints.strfloat(x))
+        f.write(pints.strfloat(x) + '\n')
+print('Found solution:' )
+# Store output
+with open('%s/%s-solution-%s-2.txt' % (savedir, saveas, fit_seed), 'w') as f:
+    for k, x in enumerate(obtained_parameters1):
+        print(pints.strfloat(x))
+        f.write(pints.strfloat(x) + '\n')
+print('Found solution:' )
+# Store output
+with open('%s/%s-solution-%s-3.txt' % (savedir, saveas, fit_seed), 'w') as f:
+    for k, x in enumerate(obtained_parameters2):
+        print(pints.strfloat(x))
+        f.write(pints.strfloat(x) + '\n')
+
+# Simple plots
+fig, axes = basic_plot_splitted(raw_data, fig=None, axes=None, c='C0', ls='')
+sol0 = model.simulate(transform_from_model_param(obtained_parameters0[:-1]))
+sol1 = model.simulate(transform_from_model_param(obtained_parameters1[:-1]))
+sol2 = model.simulate(transform_from_model_param(obtained_parameters2[:-1]))
+fig, axes = basic_plot_splitted(sol0, fig=fig, axes=axes, c='C1', ls='-')
+fig, axes = basic_plot_splitted(sol1, fig=fig, axes=axes, c='C2', ls='--')
+fig, axes = basic_plot_splitted(sol2, fig=fig, axes=axes, c='C3', ls=':')
+plt.subplots_adjust(hspace=0)
+plt.savefig('%s/%s-solution-%s.png' % (savedir, saveas, fit_seed),
+        bbox_inches='tight')
+plt.close()
--- model.py
+++ model.py
@@ -14,13 +14,16 @@ class FirstOrderLeakyTransmissionLineNetwork(object):
     [1] Vanpoucke FJ, et al., 2004, IEEE Trans Biomed Eng.
     """
 
-    def __init__(self, n_electrodes=16):
+    def __init__(self, n_electrodes=16, transform=None):
         """
         Optional input
         =====
         `n_electrodes`: Number of electrodes, default = 16.
+        `transform`: Transformation of search space parameters to model
+                     parameters.
         """
         self.n_e = n_electrodes
+        self.transform = transform
 
         # Set up arithmetic matrix
         self.A = np.matrix(np.zeros((self.n_e, self.n_e + self.n_e - 1)))
@@ -45,8 +48,11 @@ class FirstOrderLeakyTransmissionLineNetwork(object):
         `parameters`: [R_T1, R_T2, ..., R_L1, R_L2, ...], where `R_Ti` is the
                       transversal resistance along the i^th electrodes, and 
                       `R_Li` is the longitudinal resistance between the i^th
-                      and the (i+1)^th electrodes.
+                      and the (i+1)^th electrodes. Parameter units are all in
+                      kiloOhms.
         """
+        if self.transform is not None:
+            parameters = self.transform(parameters)
         rt = np.asarray(parameters[:self.n_e], dtype=np.float)
         rl = np.asarray(parameters[self.n_e:], dtype=np.float)
 
@@ -59,7 +65,31 @@ class FirstOrderLeakyTransmissionLineNetwork(object):
         G[[self.n_e + np.arange(self.n_e - 1),
             self.n_e + np.arange(self.n_e - 1)]] = 1. / rl
 
+        # try: ? 
         return np.linalg.inv(self.A * G * self.A.T)
+
+
+#
+# Parameter transformation
+#
+def log_transform_from_model_param(param):
+    # Apply natural log transformation to model parameters
+    out = np.copy(param)
+    out[:] = np.log(out[:])
+    return out
+
+
+def log_transform_to_model_param(param):
+    # Inverse of log_transform_from_model_param()
+    # Apply natural exp transformation to model parameters
+    out = np.copy(param)
+    out[:] = np.exp(out[:])
+    return out
+
+
+def donothing(param):
+    out = np.copy(param)
+    return out
 
 
 #
@@ -81,11 +111,20 @@ class GaussianLogLikelihood(pints.LogPDF):
     [1] Clerx M, et al., 2019, JORS.
     """
     def __init__(self, model, values, mask=None):
+        """
+        Input
+        =====
+        `model`: The cochlea model, following the ForwardModel requirements in
+                 PINTS.
+        `values`: The data that match the output of the `model` simulation.
+        `mask`: A function that takes in the data and replace undesired 
+                entries with `nan`.
+        """
         super(GaussianLogLikelihood, self).__init__()
 
         self._model = model
         self._values = values
-        seff._mask = mask
+        self._mask = mask
 
         # Store counts
         self._np = model.n_parameters() + 1  # assume all has the same sigma
@@ -95,17 +134,102 @@ class GaussianLogLikelihood(pints.LogPDF):
         self._logn = 0.5 * self._nt * np.log(2 * np.pi)
 
     def n_parameters(self):
+        """
+        Return number of parameters.
+        """
         return self._np
 
     def __call__(self, x):
-        error = self._values - self._model.simulate(x, self._times)
-        return np.sum(self._offset + self._multip * np.sum(error**2, axis=0))
-
-    def __call__(self, x):
+        """
+        Return the computed Gaussian log-likelihood for the given parameters
+        `x`.
+        """
         sigma = x[-1]
         error = self._values - self._model.simulate(x[:-1])
         if self._mask is not None:
             error = self._mask(error)  # error may contain nan.
         return np.sum(- self._logn - self._nt * np.log(sigma)
                       - np.nansum(error ** 2) / (2 * sigma ** 2))
+
+
+#
+# Log-prior
+#
+class UniformLogPrior(pints.LogPrior):
+    """
+    Unnormalised uniform prior for the cochlea model for PINTS [1].
+
+    [1] Clerx M, et al., 2019, JORS.
+    """
+    def __init__(self, n_electrodes=16, transform=donothing,
+            inv_transform=donothing):
+        """
+        Optional input
+        =====
+        `n_electrodes`: Number of electrodes, default = 16.
+        `transform`: Parameter transformation to model parameters.
+        `inv_transform`: Parameter transformation from model parameters.
+        """
+        super(UniformLogPrior, self).__init__()
+
+        self.n_e = n_electrodes
+
+        self.lower = np.array(
+                [5.0] * (self.n_e - 1)  # R_T, kOhms
+                + [1.0]  # R_basel, kOhms
+                + [0.01] * (self.n_e - 1)  # R_L, kOhms
+                )
+
+        self.upper = np.array(
+                [50.0] * (self.n_e - 1)  # R_T, kOhms
+                + [10.0]  # R_basel, kOhms
+                + [1.6] * (self.n_e - 1)  # R_L, kOhms
+                )
+
+        self.minf = -float('inf')
+
+        self.transform = transform
+        self.inv_transform = inv_transform
+
+    def n_parameters(self):
+        """
+        Return number of parameters.
+        """
+        return self.n_e + self.n_e - 1
+
+    def __call__(self, parameters):
+        """
+        Return the computed unnormalised log-prior for the given `parameters`.
+        """
+        debug = False
+        parameters = self.transform(parameters)
+
+        # Check parameter boundaries
+        if np.any(parameters < self.lower):
+            if debug: print('Lower')
+            return self.minf
+        if np.any(parameters > self.upper):
+            if debug: print('Upper')
+            return self.minf
+
+        # Return
+        return 0
+
+    def sample(self, n=1):
+        """
+        Return `n` parameter samples, sampled from this log-prior.
+        """
+        out = np.zeros((n, self.n_parameters()))
+
+        for i in range(n):
+            p = np.zeros(self.n_parameters())
+
+            p[:] = self.transform(np.random.uniform(
+                self.inv_transform(self.lower[:-1]),
+                self.inv_transform(self.upper[:-1])))
+
+            out[i, :] = self.inv_transform(p)
+
+        # Return
+        return out
 
