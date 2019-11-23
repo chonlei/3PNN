c2a0550deb317a0589ba945ddc35452d1c168cca
Chon Lok Lei <chonloklei@gmail.com>
2019-11-23 16:06:53 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cochlea-efi',
+    version='0.0.1',
+    description='Modelling choclea EFI',
+    license='BSD 3-clause license',
+    maintainer='Chon Lok Lei',
+    maintainer_email='chon.lei@cs.ox.ac.uk',
+    packages=find_packages(include=('method')),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'scikit-learn',
+        'tensorflow',
+    ],
+)
+
