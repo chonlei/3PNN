74b109236549a8314817149cdc6c2ced0f212d16
iekmanlei <55033708+iekmanlei@users.noreply.github.com>
2021-07-06 16:32:31 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     maintainer_email='chonloklei@gmail.com',
     packages=find_packages(include=('method')),
     install_requires=[
-        'tensorflow==2.2.0',
+        'tensorflow==2.1.0',
         'pints @ git+https://github.com/pints-team/pints@6e30367e07c7be0888c8e051d0e83a8cbebdb2cb#egg=pints',
         'SALib==1.4.0.1',
         'scikit-learn==0.24.0',
